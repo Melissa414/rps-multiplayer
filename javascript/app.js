4e697f5fddd46bbf5056335ca22f7ebf2ba61233
Melissa414 <Missy41491@gmail.com>
2016-09-18 13:07:01 -0400

    added jquery, fixed the css, and almost the final draft of html

--- javascript/app.js
+++ javascript/app.js
@@ -1,56 +1,103 @@
-var config = {
-    apiKey: "AIzaSyAJS4YQWU5DmESeYueG1qH1NGkjv3DncEY",
-    authDomain: "fir-click-counter-7cdb9.firebaseapp.com",
-    databaseURL: "https://fir-click-counter-7cdb9.firebaseio.com",
-    storageBucket: "fir-click-counter-7cdb9.appspot.com",
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCZkh0DajG2-PJ_CKS1LZIPjuHY1okFV_g",
+    authDomain: "rockpaperscissors-6404d.firebaseapp.com",
+    databaseURL: "https://rockpaperscissors-6404d.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "770214253444"
   };
   firebase.initializeApp(config);
 
 var database = firebase.database();
 
-var playerOne = player;
-var playerTwo = enemy;
+var player;
+var enemey;
 
-var scissorsCount = 0;
-var paperCount = 0;
-var rockCount = 0;
+var rock;
+var paper;
+var scissors;
 
-var score = 0;
 var win = 0;
 var loss = 0;
 var tie = 0;
+var score= 0;
 // console.log("ready to go")
 
-
-$(document).ready(function() {
-
+  $(document).ready(function(){
 
 
-//the outcomes if player one picks
-  function pickAnImage(result){
-    var playerOne = playerTwo[Math.floor(math.random() * playerTwo.length)];
 
-      if ((playerOne == "rock") && (playerTwo == "scissors")){
-        win++;
+  $("#player").click(function() {
 
-      }else if ((playerOne == "scissors") && (playerTwo == "paper")){
-        win++;
+    $("#paper").appendTo("firstBox");
+    $("#rock, #scissors").hide("#firstBox");
 
-      }else if ((playerOne == "paper") && (playerTwo == "rock")){
-        win++;
+  });
 
-      }else if ((playerOne == "rock") && (playerTwo == "paper")){
-        loss++;
+  $("#player").click(function() {
 
-      }else if ((playerOne == "paper") && (playerTwo == "scissors")){
-        loss++;
+      $("#rock").appendTo("#firstBox");
+      $("#scissors, #paper").hide("#firstBox");
+  });
 
-      }else if ((playerOne == "scissors") && (playerTwo == "rock")){
-        loss++;
+  $("#player").click(function() {
 
-      }else if (playerOne == playerTwo){
-        tie++;
+      $("#scissors").appendTo("#firstBox");
+      $("#rock, #paper").hide("#firstBox");
+  });
 
+  $("button").click(function(){
+
+    var rockIsPicked = function(){
+
+      if (rock == rock){
+        tie++
+      }else if(rock == paper){
+        loss++
+      }else if(rock == scissors){
+        win++
       }
+    };
+
+    var paperIsPicked = function(){
+
+      if (paper == paper){
+        tie++
+      }else if(paper == scissors){
+        loss++
+      }else if(paper == rock){
+        win++
+      }
+    };
+
+    var scissorsIsPicked = function(){
+
+      if (scissors == scissors){
+        tie++
+      }else if(scissors == rock){
+        loss++
+      }else if(scissors == paper){
+        win++
+      }
+    };
+
+
+    $("score").on(function() {
+    $("#player").html("win");
+    $("#player").html("loss");
+    $("#player").html("tie");
+    });
+
+    //get elements
+
+
+
+
+    // function(errorObject){
+    //   console.log("Errors handled: " + errorObject.code)
+    // };
+
+    return false;
+  });
 
 });
